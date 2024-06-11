9055f6aaab243a50e85b4861f3d3cf8807f01ebe
Xalmon <xalmonoyefule@gmail.com>
2024-06-11 14:37:33 +0100

    Initial commit

---
+++ src/main/java/com.example.LogApp2/LogApp2Application.java
@@ -0,0 +1,12 @@
+package com.example.LogApp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogApp2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LogApp2Application.class, args);
+  }
+}
---
+++ src/test/java/com.example.LogApp2/LogApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LogApp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LogApp2ApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
